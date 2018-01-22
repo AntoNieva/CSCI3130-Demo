8ab354325c17d41b7a12aff7c1883438e1e26e90
Liwei <menliweisammy@gmail.com>
2018-01-23 04:11:32 +0800

    added import widget and view; added button and editview in xml; added findViewById function;

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.*;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
-
+   EditText editText;
+   TextView text;
+   Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text= findViewById(R.id.viewAntoNieva);
+        button= findViewById(R.id.btnAntoNieva);
+        editText=findViewById(R.id.txtAntoNieva);
+        button.setOnClickListener(new View.OnClickListener(){
+     @Override
+            public void onClick(View view){
+         String str=editText.getText().toString();
+
+             text.setText(str);
+     }
+        });
     }
 }
